a655deed3ab64c5e4c33a762e5cb10726c7794e9
enantes <sambaeverything@gmail.com>
2022-07-19 17:02:03 +0200

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # logz
+
+To install:
+`npm install clogz`
+
+To use in React JS:
+
+`import logz from "./logs/index.js";`
---
+++ index.js
@@ -0,0 +1,3 @@
+export default function clogz(message) {
+  return console.log(`%c${message}`, "background: #222; color: #bada55");
+}
